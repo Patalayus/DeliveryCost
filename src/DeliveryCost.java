30f29440bb5cc218d3f58324b1e00ca5c0a09a18
Luke Charles <lukecharles222@gmail.com>
2018-05-10 18:17:33 +0100

    classes implemented

--- src/DeliveryCost.java
+++ src/DeliveryCost.java
@@ -1,6 +1,14 @@
 public class DeliveryCost {
     public static void main(String []args){
-        System.out.print("Please insert your charge");
+        class TypeofService{
+
+        }
+        class BasicCharge{
+
+        }
+        class ExtraChargeperUnitWeight{
+
+        }
 
     }
 }
