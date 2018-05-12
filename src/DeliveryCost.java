7e62250a3e2569bcc75f415e9b3ad0d18a7491bd
Luke Charles <lukecharles222@gmail.com>
2018-05-12 13:53:44 +0100

    validation and boolean

--- src/DeliveryCost.java
+++ src/DeliveryCost.java
@@ -28,6 +28,7 @@ public class DeliveryCost {
 
         System.out.println("By how much is your product over the weight per unit?\n1. 1 Unit\n2. 2 Units\n3. 3 Units\n4. 4 Units\n5. 5 Units\n6. Over 5 Units");
         double weightexceed = 0.0;
+        boolean check5 = false;
         int answer2 = sc.nextInt();
         if(answer1==1){
             delivery1 += 7.15;
@@ -39,6 +40,9 @@ public class DeliveryCost {
                 delivery1 += 6.0;
             }if(answer2==4){
                 delivery1 += 8.0;
+            }if(answer2==5){
+                check5 = true;
+                delivery1 += 10.00;
             }
         }else if(answer1==2){
             delivery1 += 5.00;
@@ -50,6 +54,9 @@ public class DeliveryCost {
                 delivery1 += 1.50;
             }if(answer2==4){
                 delivery1 += 2.00;
+            }if(answer2==5){
+                check5 = true;
+                delivery1 += 2.50;
             }
         }else if(answer1==3){
             delivery1 += 2.20;
@@ -61,6 +68,9 @@ public class DeliveryCost {
                 delivery1 += 0.60;
             }if(answer2==4){
                 delivery1 += 0.80;
+            }if(answer2==5){
+                check5 = true;
+                delivery1 += 1.00;
             }
         }else if(answer1==4){
             delivery1 += 6.20;
@@ -72,8 +82,17 @@ public class DeliveryCost {
                 delivery1 += 3.00;
             }if(answer2==4){
                 delivery1 += 4.00;
+            }if(answer2==5){
+                check5 = true;
+                delivery1 += 5.00;
             }
         }
-        System.out.println("Delivery Cost: £"+delivery1);
+        if(check5 == false){
+            System.out.println("Delivery Cost: £" + delivery1);
+        }else if(check5 == true){
+            System.out.println("Your delivery cost is over £"+ delivery1);
+        }else{
+            System.out.println("Please enter a valid input");
+        }
     }
 }
