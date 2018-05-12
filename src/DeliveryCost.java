e3da2256e81fe8ed15c973c5c7bebb466bca0328
Luke Charles <lukecharles222@gmail.com>
2018-05-12 13:57:55 +0100

    layout change

--- src/DeliveryCost.java
+++ src/DeliveryCost.java
@@ -16,32 +16,33 @@ public class DeliveryCost {
             public double recorded = 6.20;
         }
         class ExtraChargeperUnitWeight{
-            public double first = 2.0;
-            public double second = 0.5;
-            public double third = 0.2;
-            public double fourth = 1.0;
+            public double first = 2.00;
+            public double second = 0.50;
+            public double third = 0.20;
+            public double fourth = 1.00;
         }
         System.out.println("What type of service are you using?z\n1. Registered Delivery, 1st Class\n2. Standard 1st Class\n3. Standard 2nd Class\n4. 48 Hrs Recorded Delivery");
         Scanner sc = new Scanner(System.in);
-        double delivery1 = 0.0;
+        double delivery1 = 0.00;
         int answer1 = sc.nextInt();
 
         System.out.println("By how much is your product over the weight per unit?\n1. 1 Unit\n2. 2 Units\n3. 3 Units\n4. 4 Units\n5. 5 Units\n6. Over 5 Units");
-        double weightexceed = 0.0;
-        boolean check5 = false;
+        double weightexceed = 0.00;
+
+        int check5 = 0;
         int answer2 = sc.nextInt();
         if(answer1==1){
             delivery1 += 7.15;
             if(answer2==1){
-                delivery1 += 2.0;
+                delivery1 += 2.00;
             }if(answer2==2){
-                delivery1 += 4.0;
+                delivery1 += 4.00;
             }if(answer2==3){
-                delivery1 += 6.0;
+                delivery1 += 6.00;
             }if(answer2==4){
-                delivery1 += 8.0;
+                delivery1 += 8.00;
             }if(answer2==5){
-                check5 = true;
+                check5 = 1;
                 delivery1 += 10.00;
             }
         }else if(answer1==2){
@@ -55,7 +56,7 @@ public class DeliveryCost {
             }if(answer2==4){
                 delivery1 += 2.00;
             }if(answer2==5){
-                check5 = true;
+                check5 = 1;
                 delivery1 += 2.50;
             }
         }else if(answer1==3){
@@ -69,7 +70,7 @@ public class DeliveryCost {
             }if(answer2==4){
                 delivery1 += 0.80;
             }if(answer2==5){
-                check5 = true;
+                check5 = 1;
                 delivery1 += 1.00;
             }
         }else if(answer1==4){
@@ -83,13 +84,13 @@ public class DeliveryCost {
             }if(answer2==4){
                 delivery1 += 4.00;
             }if(answer2==5){
-                check5 = true;
+                check5 = 1;
                 delivery1 += 5.00;
             }
         }
-        if(check5 == false){
+        if(check5 == 0){
             System.out.println("Delivery Cost: £" + delivery1);
-        }else if(check5 == true){
+        }else if(check5 == 1){
             System.out.println("Your delivery cost is over £"+ delivery1);
         }else{
             System.out.println("Please enter a valid input");
